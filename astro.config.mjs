78d2745093718250158ed0606e31f3f527578143
EG <ladistillerhie@gmail.com>
2026-07-19 19:37:56 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://karate-shotokai-lacroix.netlify.app',
+  site: 'https://karate-shotokai-lacroix-falgarde.netlify.app',
   output: 'static',
   integrations: [sitemap()],
 });

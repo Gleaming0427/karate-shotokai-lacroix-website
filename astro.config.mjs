3f3f57fb2e44e66788847ae3dc0f4695e8e20d41
EG <ladistillerhie@gmail.com>
2026-07-19 17:18:07 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // Décommenter et ajuster si déploiement sur un repo projet GitHub Pages
-  // base: '/karate-shotokai-lacroix-website',
-  site: 'https://shotokailacroixfalgarde.fr',
+  // Si tu utilises un domaine personnalisé (shotokailacroixfalgarde.fr), commente la ligne base
+  base: '/karate-shotokai-lacroix-website',
+  site: 'https://gleaming0427.github.io',
   output: 'static',
   integrations: [sitemap()],
 });
